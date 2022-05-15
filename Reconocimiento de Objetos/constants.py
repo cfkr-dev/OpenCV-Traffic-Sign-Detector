032b3836ba4a3fd3a7e42b965355513cd207579d
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2022-05-15 14:53:16 +0200

    Añadido esquema para practica 2

---
+++ Reconocimiento de Objetos/constants.py
@@ -0,0 +1,9 @@
+SIGNALLIST = ['prohibicion', 'peligro', 'stop', 'direccionProhibida', 'cedaPaso', 'direccionObligatoria']
+PROHIBICION = ['00', '01', '02', '03', '04', '05', '07', '08', '09', '10', '15', '16']
+PELIGRO = ['11', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+STOP = ['14']
+DIRECCIONPROHIBIDA = ['17']
+CEDAPASO = ['13']
+DIRECCIONOBLIGATORIA = ['38']
+TRAIN_PATH = ''
+TEST_PATH = ''
